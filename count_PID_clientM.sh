c37457ed3962c1d906d30b220a74af453707a05f
root <root@ip-172-31-81-126.ec2.internal>
2019-03-12 11:30:38 +0000

    3

---
+++ count_PID_clientM.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script displays the PIDs and count of matching processes.
+
+PROC_NAME="${1}"
+Server_name="${2}"
+PIDS=$(ssh ${Server_name} pidof ${PROC_NAME})
+COUNT=$(echo $PIDS | wc -w)
+
+if [[ "${COUNT}" -gt 0 ]]
+then
+  echo "PROCESS NAME: ${PROC_NAME}"
+  echo "COUNT: ${COUNT}"
+  echo "PID(S): ${PIDS}"
+else
+  echo "No processes named ${PROC_NAME} found." >&2
+  exit 1
+fi
+
